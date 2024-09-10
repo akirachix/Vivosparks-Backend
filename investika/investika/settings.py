c6e5c2af2ad00f7296993f51cd5007c80089b25f
SusanFavour <susanfavourodongo@gmail.com>
2024-09-10 10:15:07 +0300

    added postgresql database

--- investika/investika/settings.py
+++ investika/investika/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'investika.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
 
