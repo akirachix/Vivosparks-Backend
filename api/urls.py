db91cd635679395b251bfa7407fcebe49707d7f0
majidhawa <majidhawaah@gmail.com>
2024-09-14 17:09:50 +0300

    Add Swagger documentation

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
 from rest_framework import permissions
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API Title",
+        default_version='v1',
+        description="API documentation for your project",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 from .views import (
@@ -61,6 +75,10 @@ urlpatterns = [
    # URLs for achievement-related views
    path('achievements/', AchievementView.as_view(), name='achievement-list'),  # List all achievements
    path('achievements/<int:id>/', AchievementDetailView.as_view(), name='achievement-detail'),  # View details of a specific achievement by ID
+   
+   
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 
 ]
